819fa6a2a9c93a2c21abe27986ff576c6f4eb93b
junjun160504 <vydung0504@gmail.com>
2025-09-29 10:27:48 +0700

    anh quang cc

--- junjun/src/api/chatbot.js
+++
@@ -1,4 +0,0 @@
-import api from './index';
-
-// Gửi tin nhắn đến chatbot
-export const chatWithBot = (message) => api.post('/chatbot', { message });
--- junjun/src/api/review.js
+++
@@ -1,4 +0,0 @@
-import api from './index';
-
-// Gửi đánh giá của khách
-export const submitReview = (reviewData) => api.post('/review', reviewData);
